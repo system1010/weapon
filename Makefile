7ed76a6982df336a23fc867d41df882fdf7787da
blueeagle <32022861+system1010@users.noreply.github.com>
2020-02-22 17:53:01 +0500

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,12 @@
+#
+# Makefile
+# root, 2020-02-22 12:23
+#
+
+all:
+	@echo "Makefile needs your attention"
+	g++ weapon.cpp
+
+# vim:ft=make
+#
+
---
+++ weapon.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+
+class Weapon{
+	public:
+  char *Shoot;
+  friend ostream& operator<<(ostream&, Weapon&);
+
+};
+
+ostream& operator << (ostream& out, Weapon& w)
+{
+    out << w.Shoot << endl;
+    return out;
+}
+
+class Knife: public Weapon{
+	public:
+		Knife(){
+    Shoot = "VJUM!!!";
+    }
+
+};
+class Gun: public Weapon
+{
+	public:
+		Gun(){
+  	Shoot= "BANG!";
+		}
+};
+
+class SubmashineGun: public Weapon
+{
+	public:
+	SubmashineGun(){
+	Shoot= "BANG! BANG! BANG!";
+	}
+};
+
+class Bazooka: public Weapon
+{
+	public:
+	Bazooka(){
+	Shoot= "BADABUB!!!";
+	}
+};
+
+class Player{
+	public:
+  void Shoot(Weapon *weapon){
+	cout << *weapon; 
+	}
+};
+
+
+int main(){
+
+int tmp=0;
+cout <<"0||1||2||3";
+scanf("%d",&tmp);
+
+Weapon *w=0;
+switch ( tmp ) {
+	case 0:
+ 		w=new Knife;
+ 		break;
+	case 1:
+		w=new Gun;
+		break;
+	case 2:
+		w=new SubmashineGun;
+		break;
+	case 3:
+		w=new Bazooka;
+		break;
+	default:
+ break;
+}  
+
+
+Player player;
+
+
+player.Shoot(w);
+
+return 0;
+
+}
+
+
+
+
